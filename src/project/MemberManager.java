5239ca7130459e3bfd518cfd4f33caa2d346b2d1
joonwu <wnsdn4875@gmail.com>
2022-05-04 21:50:22 +0900

    사용자 정의 예외 삭제

--- src/project/MemberManager.java
+++ src/project/MemberManager.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class MemberManager {
     private static final Scanner sc = new Scanner(System.in);
 
-    static void readMenu() {
+    static void readMenu() throws SQLException {
         while (true) {
             System.out.print("""
                     목록을 원하시면 1번을 입력하세요.
@@ -41,7 +41,6 @@ public class MemberManager {
     }
 
 
-
     //회원 목록 조회
     public static void getMemberList() {
         MemberDAO dao = new MemberDAO();
@@ -56,51 +55,43 @@ public class MemberManager {
     }
 
     //회원 등록
-    public static void insertMember() {
+    public static void insertMember() throws SQLException {
         MemberVO vo = new MemberVO();
         MemberDAO dao = new MemberDAO();
 
-        try {
-            System.out.print("아이디를 입력하세요. (형식 M-00001):");
-            String br = sc.nextLine();
-            String memberID = sc.nextLine();
+        System.out.print("아이디를 입력하세요. (형식 M-00001):");
+        String br = sc.nextLine();
+        String memberID = sc.nextLine();
 
-            if (idExistsCheck(memberID)) {
-                System.out.printf("%s가 이미 존재합니다.\n", memberID);
-            } else if (memberID == ""){
-                System.out.println("아이디는 필수입력 항목입니다.");
-            }
-            else if (IdFormCheck(memberID)) {
-                vo.setMemberID(memberID);
-                try {
-                    System.out.print("이름을 입력하세요 :");
-                    String name = sc.nextLine();
-                    if (name == ""){
-                        System.out.println("이름은 필수 입력 항목입니다.");
-                    } else {
-                        vo.setName(name);
-                        System.out.print("전화번호를 입력하세요 :");
-                        String phoneNumber = sc.nextLine();
-
-                        if (phoneNumber == "") {
-                            System.out.println("전화번호는 필수입력 항목입니다.");
-                        } else if (!PhoneNumberCheck(phoneNumber)) {
-                            System.out.println("전화번호는 두 개의 '-'를 포함하여 총 13개의 문자로 구성해야 합니다.");
-                        } else {
-                            vo.setPhoneNumber(phoneNumber);
-                            dao.insertMember(vo);
-                            System.out.println("---> 회원가입에 성공하셨습니다.");
-                        }
-                    }
-                } catch (Exception e) {
-                    System.out.println("이름은 필수입력 항목입니다.");
-                }
-            } else {
-                System.out.println("아이디는 'M-'로 시작해야 하며, M-를 포함하여 7개의 문자로 구성해야 합니다.");
-            }
-        } catch (Exception e) {
+        if (idExistsCheck(memberID)) {
+            System.out.printf("%s가 이미 존재합니다.\n", memberID);
+        } else if (memberID == "") {
             System.out.println("아이디는 필수입력 항목입니다.");
+        } else if (IdFormCheck(memberID)) {
+            vo.setMemberID(memberID);
+            System.out.print("이름을 입력하세요 :");
+            String name = sc.nextLine();
+            if (name == "") {
+                System.out.println("이름은 필수 입력 항목입니다.");
+            } else {
+                vo.setName(name);
+                System.out.print("전화번호를 입력하세요 :");
+                String phoneNumber = sc.nextLine();
+
+                if (phoneNumber == "") {
+                    System.out.println("전화번호는 필수입력 항목입니다.");
+                } else if (!PhoneNumberCheck(phoneNumber)) {
+                    System.out.println("전화번호는 두 개의 '-'를 포함하여 총 13개의 문자로 구성해야 합니다.");
+                } else {
+                    vo.setPhoneNumber(phoneNumber);
+                    dao.insertMember(vo);
+                    System.out.println("---> 회원가입에 성공하셨습니다.");
+                }
+            }
+        } else {
+            System.out.println("아이디는 'M-'로 시작해야 하며, M-를 포함하여 7개의 문자로 구성해야 합니다.");
         }
+
     }
 
     //회원 정보 수정
