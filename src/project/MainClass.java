277ab8cb25efaeebfd5620a26e1282975ea12e7d
joonwu <wnsdn4875@gmail.com>
2022-05-05 10:14:02 +0900

    Update MainClass

--- src/project/MainClass.java
+++ src/project/MainClass.java
@@ -1,25 +1,27 @@
 package project;
 
 public class MainClass {
-    public static void main(String[] args) throws Exception {
-
-        MemberManager memberManager = new MemberManager();
-
-
+    public static void startProgram() {
         System.out.print("""
                 #############################
                 ### 회원 관리 프로그램 START ###
                 #############################
                 """);
+    }
 
-        memberManager.readMenu();
-
-
+    public static void endProgram() {
         System.out.print("""
                 #############################
                 ### GOOD-BYE 프로그램 종료 ###
                 #############################
                 """);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        startProgram();
+        MemberManager.readMenu();
+        endProgram();
 
 
     }
